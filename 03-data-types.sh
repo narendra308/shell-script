f28a8f0e7dc034d04b0bf15b71f6ae16a1383338
narendramogili <narendramogili308@gmail.com>
2025-02-15 21:27:47 +0530

    script added

--- 03-data-types.sh
+++ 03-data-types.sh
@@ -2,5 +2,9 @@
 
 PERSON1=$1
 PERSON2=$2
+TIMESTAMP=$(date)
+echo "script executed at:$TIMESTAMP"
 
-SUM=($NUMBER1+$NUMBER2)
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "sum of $NUMBER1 and $NUMBER2 is: $sum"
