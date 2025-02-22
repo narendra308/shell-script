a8a54bf2aa62449fbb866815cab00b1e1622d4ce
narendramogili <narendramogili308@gmail.com>
2025-02-22 10:00:36 +0530

    special-variables

--- 04-arrays.sh
+++ 04-arrays.sh
@@ -1,8 +1,8 @@
-#!/bin/bash/
+#!/bin/bash
 
  MOVIES=("devara" "jani" "pushpa")
   echo "First movie: ${MOVIES[0]}"
   echo "Second movie: ${MOVIES[1]}"
-  echo "Third movie: ${MOVIES[3]}"
+  echo "Third movie: ${MOVIES[2]}"
 
-  echo "All movies are: ${movies[@]}
+  echo "All movies are: ${MOVIES[@]}"
